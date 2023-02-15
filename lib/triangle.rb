23758a42f8485e6a196441aae743aa8eae03b232
Daniel Njuguna <kagudante@gmail.com>
2023-02-15 10:54:19 +0300

    Completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_reader :w, :y, :z
+  def initialize(w, y, z)
+    @w = w
+    @y = y
+    @z = z
+  end
+
+  def kind
+    validate_triangle
+    if w == y && y == z
+      :equilateral
+    elsif w == y || y == z || w == z
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate_triangle
+    real_triangle = [(w + y > z), (w + z > y), (y + z > w)]
+    [w, y, z].each do |side|
+      real_triangle << false if side <= 0 
+    raise TriangleError if real_triangle.include?(false)
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
